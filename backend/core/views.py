e7bc7086a9cda179774cadb20e1f7a541bfc4bb2
bobby-customate <bstearman@customate.net>
2022-11-08 11:44:53 +0000

    Module 3 complete

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from json import JSONDecodeError
+from django.http import JsonResponse
+from .serializers import ContactSerializer
+from rest_framework.parsers import JSONParser
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
-# Create your views here.
+
+
+class ContactViewSet(viewsets.GenericViewSet):
+    """
+    A simple ViewSet for creating contact entires.
+    """
+    serializer_class = ContactSerializer
+
+    def create(self, request):
+        try:
+            data = JSONParser().parse(request)
+            serializer = ContactSerializer(data=data)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response(serializer.data)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except JSONDecodeError:
+            return JsonResponse({"result": "error","message": "Json decoding error"}, status= 400)
--- backend/drf_course/urls.py
+++ backend/drf_course/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from django.contrib import admin
+from core import views as core_views
 from rest_framework import routers
 
+
 router = routers.DefaultRouter()
+router.register(r'contact', core_views.ContactViewSet, basename='contact') #new endpoint for '/contact/'
 
 urlpatterns = router.urls
 
