3945f15ab410a256e35d859fcc7ec773e2030418
bobby-customate <bstearman@customate.net>
2022-11-08 11:46:09 +0000

    Module 4 complete

--- backend/core/tests.py
+++ backend/core/tests.py
@@ -1,3 +1,93 @@
-from django.test import TestCase
+from . models import Contact
+from rest_framework.test import APIClient
+from rest_framework.test import APITestCase
+from rest_framework import status
 
-# Create your tests here.
+
+
+class ContactTestCase(APITestCase):
+
+    """
+    Test suite for Contact
+    """
+    def setUp(self):
+        self.client = APIClient()
+        self.data = {
+            "name": "Billy Smith",
+            "message": "This is a test message",
+            "email": "billysmith@test.com"
+        }
+        self.url = "/contact/"
+
+    def test_create_contact(self):
+        '''
+        test ContactViewSet create method
+        '''
+        data = self.data
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Contact.objects.count(), 1)
+        self.assertEqual(Contact.objects.get().title, "Billy Smith")
+
+    def test_create_contact_without_name(self):
+        '''
+        test ContactViewSet create method when name is not in data
+        '''
+        data = self.data
+        data.pop("name")
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    
+    def test_create_contact_when_name_equals_blank(self):
+        '''
+        test ContactViewSet create method when name is blank
+        '''
+        data = self.data
+        data["name"] = ""
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_contact_without_message(self):
+        '''
+        test ContactViewSet create method when message is not in data
+        '''
+        data = self.data
+        data.pop("message")
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    
+    def test_create_contact_when_message_equals_blank(self):
+        '''
+        test ContactViewSet create method when message is blank
+        '''
+        data = self.data
+        data["message"] = ""
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_contact_without_email(self):
+        '''
+        test ContactViewSet create method when email is not in data
+        '''
+        data = self.data
+        data.pop("email")
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    
+    def test_create_contact_when_email_equals_blank(self):
+        '''
+        test ContactViewSet create method when email is blank
+        '''
+        data = self.data
+        data["email"] = ""
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_contact_when_email_equals_non_email(self):
+        '''
+        test ContactViewSet create method when email is not email
+        '''
+        data = self.data
+        data["email"] = "test"
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
