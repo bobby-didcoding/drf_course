f4e20b7eba631d816ecf8c89525f3e3b7ec184cc
bobby-customate <bstearman@customate.net>
2022-11-08 12:19:42 +0000

    Module 7 complete

--- backend/drf_course/urls.py
+++ backend/drf_course/urls.py
@@ -1,15 +1,19 @@
 from django.urls import path
 from django.contrib import admin
 from core import views as core_views
+from ecommerce import views as ecommerce_views
 from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
 
+
 router = routers.DefaultRouter()
-router.register(r'contact', core_views.ContactViewSet, basename='contact') #new endpoint for '/contact/'
+router.register(r'contact', core_views.ContactViewSet, basename='contact')
+router.register(r'item', ecommerce_views.ItemViewSet, basename='item')
+router.register(r'order', ecommerce_views.OrderViewSet, basename='order')
 
 urlpatterns = router.urls
 
 urlpatterns += [
     path('admin/', admin.site.urls),
-    path('api-token-auth/', obtain_auth_token), #gives us access to token auth
+    path('api-token-auth/', obtain_auth_token),
 ]
