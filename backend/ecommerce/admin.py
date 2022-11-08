62603ed64deec56f5cfde75e590d204b5e596168
bobby-customate <bstearman@customate.net>
2022-11-08 12:17:39 +0000

    Module 6 complete

--- backend/ecommerce/admin.py
+++ backend/ecommerce/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'item')
